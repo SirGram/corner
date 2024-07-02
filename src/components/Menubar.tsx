17c8076e8243aeadfcd164e2f9592c8d87f93d61
sirgram <gabrieulogio@gmail.com>
2024-07-02 12:19:41 +0200

    click outside menubar

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,16 +1,20 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { FaHome, FaProjectDiagram, FaInfoCircle, FaCog } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useGlobalContext } from "../context/GlobalContext";
 import { Language, Theme } from "../types/types";
 
 export default function Menubar() {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
-  const [isExtended, setIsExtended] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const { setTheme, theme, language, changeLanguage } = useGlobalContext();
+
+  const [isExtended, setIsExtended] = useState(false);
+  const menubarRef = useRef<HTMLDivElement>(null);
+
+  const cogButtonRef = useRef<HTMLButtonElement>(null);
 
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
@@ -36,6 +40,25 @@ export default function Menubar() {
     else if (hash === "about") setActiveIndex(2);
   }, [location]);
 
+  // Set isExtended to false when clicking outside of the menubar if isExtended is true
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menubarRef.current &&
+        !menubarRef.current.contains(event.target as Node) &&
+        cogButtonRef.current &&
+        !cogButtonRef.current.contains(event.target as Node)
+      ) {
+        setIsExtended(false);
+        console.log("Clicked outside, menubar is now closed.");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   const getBackgroundStyle = () => {
     const index = hoverIndex !== null ? hoverIndex : activeIndex;
     return {
@@ -51,7 +74,7 @@ export default function Menubar() {
 
   const handleClick = () => {
     setIsSpinning(true);
-    setIsExtended(!isExtended);
+    setIsExtended(prevState => !prevState);
     setTimeout(() => {
       setIsSpinning(false);
     }, 500);
@@ -61,7 +84,10 @@ export default function Menubar() {
     <nav className="fixed top-2 flex justify-center w-full items-start text-lg z-30">
       <div className="flex flex-col items-center">
         <div className="flex items-center">
-          <div className="flex gap-3 px-1 bg-base-100 dark:bg-darkBase-100 rounded-base border-base border-border dark:border-darkBorder bg-base relative overflow-hidden">
+          <div
+            ref={menubarRef}
+            className="flex gap-3 px-1 bg-base-100 dark:bg-darkBase-100 rounded-base border-base border-border dark:border-darkBorder bg-base relative overflow-hidden"
+          >
             <div
               className="absolute bg-base-200 dark:bg-darkBase-200 transition-all duration-300 ease-out rounded-base h-full"
               style={getBackgroundStyle()}
@@ -107,10 +133,11 @@ export default function Menubar() {
             </a>
           </div>
           <button
+            ref={cogButtonRef}
             onClick={handleClick}
             className="ml-2 p-2 bg-base-100 dark:bg-darkBase-100 rounded-base border-base border-border dark:border-darkBorder hover:bg-base-200 dark:hover:bg-darkBase-200 transition-colors duration-300"
           >
-            <FaCog className={isSpinning ? 'animate-spin' : ''} />
+            <FaCog className={isSpinning ? "animate-spin" : ""} />
           </button>
         </div>
         <div className="relative w-full">
@@ -128,7 +155,7 @@ export default function Menubar() {
                 onChange={(e) => setTheme(e.target.value as Theme)}
                 className="p-2 w-full bg-base-200 hover:bg-base-300 dark:bg-darkBase-200 dark:hover:bg-darkBase-300 rounded-base"
               >
-                <option value="light" >Light</option>
+                <option value="light">Light</option>
                 <option value="dark">Dark</option>
               </select>
               <select
