054d0c5a2d53cd8a979c005d311e6d492567cc7a
SirGram <sirgramcg@gmail.com>
2024-07-12 19:02:10 +0200

    css small fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,24 @@
-export default function Button({ href, text }: { href?: string; text: string }) {
+export default function Button({
+  href,
+  text,
+}: {
+  href?: string;
+  text: string;
+}) {
   if (href) {
     return (
       <a
         href={href}
         target="_blank"
         rel="noopener noreferrer"
-        className="px-4 h-10 dark:hover:bg-base-200 hover:bg-base-200 transition-colors duration-300 w-full border-border border-base dark:border-darkBorder rounded-base flex items-center justify-center"
+        className="px-4 h-10 dark:hover:bg-darkBase-200 hover:bg-base-200  transition-colors duration-300 w-full border-border border-base dark:border-darkBorder rounded-base flex items-center justify-center"
       >
         {text}
       </a>
     );
   } else {
     return (
-      <button className="px-4 h-10 dark:hover:bg-base-200 hover:bg-base-200 transition-colors duration-300 w-full border-border border-base dark:border-darkBorder rounded-base flex items-center justify-center">
+      <button className="px-4 h-10 dark:hover:bg-darkBase-200 hover:bg-base-200  transition-colors duration-300 w-full border-border border-base dark:border-darkBorder rounded-base flex items-center justify-center">
         {text}
       </button>
     );
